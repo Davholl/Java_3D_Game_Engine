cf5b74d01faac16f43901075a7da75e0f000b5d2
Davi Hollanda <dav-holl@hotmail.com>
2020-08-25 21:25:25 -0300

    renderizando um retângulo

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -3,19 +3,42 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 	
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		//vertices de um retângulo de exemplo para testar
+		float[] vertices = {
+				//Triangulo de baixo
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				
+				//Triangulo de cima
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
 			//logic
 			//render
+			renderer.render(model);
 			DisplayManager.updateDisplay();
-			Display.setTitle("Janelina");
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 
