37b2230593a1fbbc63a67d3d30db6f4e6bb9f288
Davi Hollanda <dav-holl@hotmail.com>
2020-08-26 21:30:04 -0300

    aplicando uma textura simples no retângulo

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 	
@@ -33,7 +35,16 @@ public class MainGameLoop {
 				3, 1 , 2
 		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		float[] textureCoords = {
+				0,0, //v1
+				0,1, //v1
+				1,1, //v2
+				1,0  //v3
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("parrot"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()) {
 			
@@ -42,7 +53,7 @@ public class MainGameLoop {
 			//render
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
