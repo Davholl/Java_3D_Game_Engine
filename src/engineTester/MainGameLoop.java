54ffa0bd9537515e2ec2ea016aeca7ece2bb9386
Davi Hollanda <dav-holl@hotmail.com>
2020-08-28 00:21:39 -0300

    criando 'camera' com matrizes de vizualização e projeção

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -13,47 +16,112 @@ import textures.ModelTexture;
 public class MainGameLoop {
 	
 	public static void main(String[] args) {
+		
 		DisplayManager.createDisplay();
-		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		//vertices de um retangulo de exemplo para testar
+		//vertices de um cubo de exemplo
 		//Os vertices dos triangulos são lidos no sentido antihorario
-		float[] vertices = {
-				//Os vertices totais do retângulo
-				-0.5f, 0.5f, 0f, //v0
-				-0.5f, -0.5f, 0f,//v1
-				 0.5f, -0.5f, 0f,  //v2
-				 0.5f, 0.5f, 0f //v3
-		};
-		
-		//Especificando os vertices que vão compor os 2 triangulos
-		int[] indices = {
-				0, 1, 3,
-				3, 1 , 2
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0,0, //v1
-				0,1, //v1
-				1,1, //v2
-				1,0  //v3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		
 		ModelTexture texture = new ModelTexture(loader.loadTexture("parrot"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
+		Entity entity = new Entity(texturedModel, new Vector3f(0f, 0f, -1f), 0, 0, 0, 1);
+		
+		Camera camera = new Camera();
+		
 		while(!Display.isCloseRequested()) {
-			
+			entity.increaseRotation(1f, 1f, 0f);
 			//logic
 			
 			//render
 			renderer.prepare();
+			camera.move();
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
